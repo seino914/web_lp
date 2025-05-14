e50ffcf1c5a43bdf0dbc1d4052f45843f5dcf3e5
Tonosaki <tonosaki914@icloud.com>
2025-05-14 13:05:35 +0900

    メタデータの修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "WebPro",
+  title:
+    "WebPro | Web制作から開発、マーケティングまでプロのエンジニアにお任せ！",
   description: "Web制作から開発、マーケティングまで幅広いサービスを提供します",
 };
 
